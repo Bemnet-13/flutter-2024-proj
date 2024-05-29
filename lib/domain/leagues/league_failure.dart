0d4bb45793c485df21100fce59d2f7569ddd3daf
techkelster <techkelster@gmail.com>
2024-05-29 17:35:57 +0300

    league failures

---
+++ lib/domain/leagues/league_failure.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'league_failure.freezed.dart';
+
+@freezed
+abstract class LeagueFailure with _$LeagueFailure {
+  const factory LeagueFailure.unexpected() = Unexpected;
+  const factory LeagueFailure.insufficientPermissions() =
+      InsufficientPermissions;
+  const factory LeagueFailure.unableToUpdate() = UnableToUpdate;
+  const factory LeagueFailure.notFound() = NotFound;
+  const factory LeagueFailure.invalidData() = InvalidData;
+}
