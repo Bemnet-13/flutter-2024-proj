69df7fe2e96e4e8db9ca4ca9bd4b6ae973b88711
Abzaek <abzaek@hotmail.com>
2024-05-30 23:45:42 +0300

    added skeleton structure

--- lib/presentation/screens/manage_league.dart
+++ lib/presentation/screens/manage_league.dart
@@ -1,11 +1,13 @@
+import 'package:FantasyE/presentation/widgets/appbar.dart';
+import 'package:FantasyE/presentation/widgets/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -19,6 +21,49 @@ class ManageLeagues extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+        appBar: const CustomBar(title: 'Manage leauges'),
+        drawer: const Drawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(12),
+          child: ListView.builder(
+            itemCount: 6,
+            itemBuilder: (context, index) => const Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Padding(
+                      padding: EdgeInsets.only(left: 24.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Meta League',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Total players: 10',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    subtitle: Padding(
+                      padding: EdgeInsets.only(left: 24.0, top: 15),
+                      child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start),
+                    ),
+                  ),
+                ),
+                Divider(),
+              ],
+            ),
+          ),
+        ));
   }
 }
