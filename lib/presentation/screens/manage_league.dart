39d05b32e4146bdc95b1636bf490b065492551ec
Abzaek <abzaek@hotmail.com>
2024-05-31 06:24:06 +0300

    changed some bit of style

--- lib/presentation/screens/manage_league.dart
+++ lib/presentation/screens/manage_league.dart
@@ -1,5 +1,4 @@
 import 'package:FantasyE/presentation/widgets/appbar.dart';
-import 'package:FantasyE/presentation/widgets/buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -32,31 +31,23 @@ class ManageLeagues extends StatelessWidget {
               children: [
                 Padding(
                   padding: EdgeInsets.all(8.0),
-                  child: ListTile(
-                    title: Padding(
-                      padding: EdgeInsets.only(left: 24.0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Meta League',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            'Total players: 10',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    ),
-                    subtitle: Padding(
-                      padding: EdgeInsets.only(left: 24.0, top: 15),
-                      child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.start),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 24.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Meta League',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Total players: 10',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ],
                     ),
                   ),
                 ),
